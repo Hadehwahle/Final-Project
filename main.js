a434ae20e0193f78a0b62e2505b3401d0ef85dff
Adeleke <adelekepeterpierre@gmail.com>
2024-03-15 02:43:08 +0100

    added a toggle function

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const menu = document.querySelector("#menu-links");
 const btn = document.querySelector(".bar");
+const changeIcon = document.querySelector("#bars");
 
 btn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
   menu.classList.toggle("show");
+  changeIcon.classList.toggle("fa-xmark");
 }
